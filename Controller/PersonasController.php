d6faa28d1585e53ecd769d8363cb7984f316d7f9
JEYBAN37 <jeyban37@gmail.com>
2026-05-05 09:18:24 -0500

    jeysgegu refactor de metodo de personas

--- Controller/PersonasController.php
+++ Controller/PersonasController.php
@@ -309,5 +309,55 @@ class PersonasController extends AppController
 
 	
 	}
+
+	public function buscarPersona()
+    {
+        $this->autoRender = false;
+        $this->response->type('json');
+
+        $term = trim($this->request->query('q'));
+
+        if (strlen($term) < 3) {
+            echo json_encode([]);
+            return;
+        }
+
+        $this->loadModel('Persona');
+
+        $rows = $this->Persona->find('all', [
+            'conditions' => [
+                'OR' => [
+                    'Persona.numerodoc LIKE' => '%' . $term . '%',
+                ]
+            ],
+            'fields' => [
+                'Persona.numerodoc',
+                'Persona.primernombre',
+                'Persona.primerapellido',
+                'Persona.familia_id',
+                'Persona.sociambiental_id'
+            ],
+            'limit' => 1,
+            'recursive' => -1,
+            'order' => [
+                'Persona.numerodoc' => 'ASC'
+            ]
+        ]);
+
+        $result = [];
+
+        foreach ($rows as $row) {
+            $p = $row['Persona'];
+
+            $result[] = [
+                'cedula' => $p['numerodoc'],
+                'nombre' => $p['primernombre'] . ' ' . $p['primerapellido'],
+                'familia_id' => $p['familia_id'],
+                'sociambiental_id' => $p['sociambiental_id']
+            ];
+        }
+
+        echo json_encode($result);
+    }
 	
 }
